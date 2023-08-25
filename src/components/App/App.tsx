79c359000b759070446d2a9b98e9f050f5f396dd
Andrey <antonov.andrey.10101@gmail.com>
2023-08-25 13:16:58 +0400

    feat: add GamePage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,19 +5,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Spinner from '../Spinner/Spinner';
 
 const MainPage = lazy(() => import('../../pages/MainPage/MainPage'));
-// const GamePage = lazy(() => import('../../pages/GamePage/GamePage'));
+const GamePage = lazy(() => import('../../pages/GamePage/GamePage'));
 // const Page404 = lazy(() => import('../../pages/Page404/Page404'));
 
 function App() {
   return (
     <Router>
-      <Flex justifyContent='center' flexDir='column' m='0 auto' maxWidth="1400px" p="20px">
+      <Flex justifyContent="center" flexDir="column" m="0 auto" maxWidth="1400px" p="20px">
         {/* <Header /> */}
         <Suspense fallback={<h1>Loading...</h1>}>
           <Routes>
             <Route path="/" element={<MainPage />} />
-            {/* <Route path="/:gameId" element={<GamePage />} />
-            <Route path="*" element={<Page404 />} /> */}
+            <Route path="/:gameId" element={<GamePage />} />
+            {/* <Route path="*" element={<Page404 />} />  */}
           </Routes>
         </Suspense>
       </Flex>
---
+++ src/pages/GamePage/GamePage.tsx
@@ -0,0 +1,15 @@
+import { Helmet } from 'react-helmet';
+
+function GamePage() {
+  return (
+    <>
+      <Helmet>
+        {/* <meta name="description" content={`${currentGame ? currentGame.name : 'Current Game'} - Game Shop`} /> */}
+        {/* <title>{currentGame ? currentGame.name : 'Current Game'}- Game Shop</title> */}
+      </Helmet>
+      <main>fff</main>
+    </>
+  );
+}
+
+export default GamePage;
