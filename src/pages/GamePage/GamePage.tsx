6b7ab0e4b0295af50e6be67fda6499936b775be6
Andrey <antonov.andrey.10101@gmail.com>
2023-08-26 13:01:42 +0400

    feat: add display of info about the game by cards

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,15 +1,74 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Flex, Box, Text, Title, Image } from '@mantine/core';
+import { AiFillInfoCircle } from 'react-icons/ai';
+import { Flex, Box, Text, Title, Image, createStyles, Group, Paper, SimpleGrid, rem } from '@mantine/core';
 import { Carousel } from '@mantine/carousel';
 import { Helmet } from 'react-helmet';
 import { useAppDispatch, useAppSelector } from '../../hooks/useRedux';
 import { fetchGame } from '../../slices/gamesSlice';
+import './GamePage.scss';
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    padding: `calc(${theme.spacing.xl} * 1.5)`,
+  },
+
+  value: {
+    fontSize: rem(22),
+    fontWeight: 700,
+    lineHeight: 1,
+  },
+
+  icon: {
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[4],
+  },
+
+  title: {
+    fontWeight: 700,
+    fontSize: rem(24),
+  },
+}));
+
+const icons = {
+  genre: AiFillInfoCircle,
+  publisher: AiFillInfoCircle,
+  developer: AiFillInfoCircle,
+  releaseDate: AiFillInfoCircle,
+};
+
+interface StatsGrid {
+  title: string;
+  icon: keyof typeof icons;
+  value: string;
+  diff: number;
+}
 
 function GamePage() {
   const dispatch = useAppDispatch();
-  const { currentGame } = useAppSelector((state) => state.games);
+  const { classes } = useStyles();
   const { gameId } = useParams();
+  const { currentGame } = useAppSelector((state) => state.games);
+
+  const [data, setData] = useState<StatsGrid[]>([]);
+
+  const stats = data.map((stat) => {
+    const Icon = icons[stat.icon];
+
+    return (
+      <Paper withBorder p="md" radius="md" key={stat.title}>
+        <Group position="apart">
+          <Text size="xs" color="dimmed" className={classes.title}>
+            {stat.title}
+          </Text>
+          <Icon className={classes.icon} size="1.4rem" />
+        </Group>
+
+        <Group align="flex-end" spacing="xs" mt={25}>
+          <Text className={classes.value}>{stat.value}</Text>
+        </Group>
+      </Paper>
+    );
+  });
 
   useEffect(() => {
     if (gameId) {
@@ -17,48 +76,70 @@ function GamePage() {
     }
   }, []);
 
-  console.log(currentGame);
+  useEffect(() => {
+    if (currentGame) {
+      setData([
+        { title: 'Genre', icon: 'genre', value: currentGame.genre, diff: 5 },
+        { title: 'Publisher', icon: 'publisher', value: currentGame.publisher, diff: 5 },
+        { title: 'Developer', icon: 'developer', value: currentGame.developer, diff: 5 },
+        { title: 'Release date', icon: 'releaseDate', value: currentGame.release_date, diff: 5 },
+      ]);
+    }
+  }, [currentGame]);
 
   return (
     <>
       <Helmet>
-        {/* <meta name="description" content={`${currentGame ? currentGame.name : 'Current Game'} - Game Shop`} /> */}
-        {/* <title>{currentGame ? currentGame.name : 'Current Game'}- Game Shop</title> */}
+        <meta name="description" content={`${currentGame ? currentGame.title : 'Game Library'}`} />
+        <title>{currentGame ? currentGame.title : 'Game Library'}</title>
       </Helmet>
       <main>
         {currentGame && (
           <Box>
-            <Title order={1}>{currentGame.title}</Title>
+            <Title order={1} align="right" fz="38px" mb="30px">
+              {currentGame.title}
+            </Title>
             <Flex justify="space-between" gap="50px">
-              <Image src={currentGame.thumbnail} alt={currentGame.title} fit="cover" w="100%" h="100%" />
-              <Box p="10px 0px">
-                <Title order={2}>About</Title>
-                <Text fz="xl">{currentGame.description}</Text>
-              </Box>
+              <Image
+                className="game__image"
+                src={currentGame.thumbnail}
+                alt={currentGame.title}
+                fit="cover"
+                w="100%"
+                h="100%"
+              />
+
+              <Text fz="xl" className="game__descr">
+                {currentGame.description}
+              </Text>
             </Flex>
-            <Flex justify="space-between">
-              <Box>
-                <Title order={3}>Genre: {currentGame.genre}</Title>
-                <Title order={3}>Publisher: {currentGame.publisher}</Title>
-                <Title order={3}>Developer: {currentGame.developer}</Title>
-                <Title order={3}>Release date: {currentGame.release_date}</Title>
-              </Box>
-              <Box>
-                <Title order={3}> Requirements:</Title>
-                {Object.entries(currentGame.minimum_system_requirements).map((requirement) => (
-                  <Title order={4} key={requirement[0]}>
-                    {requirement[0][0].toUpperCase() + requirement[0].slice(1)}: {requirement[1]}
-                  </Title>
-                ))}
-              </Box>
-            </Flex>
-            <Carousel maw='100%' mx="auto" withIndicators>
+
+            <div className={classes.root}>
+              <SimpleGrid
+                cols={4}
+                breakpoints={[
+                  { maxWidth: 'md', cols: 2 },
+                  { maxWidth: 'xs', cols: 1 },
+                ]}
+              >
+                {stats}
+              </SimpleGrid>
+            </div>
+            <Carousel maw="100%" mx="auto" withIndicators>
               {currentGame.screenshots.map((screenshot) => (
                 <Carousel.Slide key={screenshot.id}>
                   <Image src={screenshot.image} alt={currentGame.title} />
                 </Carousel.Slide>
               ))}
             </Carousel>
+            <Box>
+              <Title order={3}> Requirements:</Title>
+              {Object.entries(currentGame.minimum_system_requirements).map((requirement) => (
+                <Title order={4} key={requirement[0]}>
+                  {requirement[0][0].toUpperCase() + requirement[0].slice(1)}: {requirement[1]}
+                </Title>
+              ))}
+            </Box>
           </Box>
         )}
       </main>
